fd6f1b066e33f529780f745583d9d850aebf3d91
SSSS <113474082+metagamersuniverse@users.noreply.github.com>
2023-06-17 23:08:06 +0600

    Update script.js

--- script.js
+++ script.js
@@ -9,15 +9,22 @@ document.getElementById('contactForm').addEventListener('submit', function(e) {
   // Create CSV data
   const csvData = Papa.unparse([{ name, email, message }]);
 
-  // Create a data URI for the CSV file
-  const csvFile = new Blob([csvData], { type: 'text/csv' });
-  const csvURL = URL.createObjectURL(csvFile);
-
-  // Trigger the download
-  const downloadLink = document.createElement('a');
-  downloadLink.href = csvURL;
-  downloadLink.download = 'data.csv';
-  document.body.appendChild(downloadLink);
-  downloadLink.click();
-  document.body.removeChild(downloadLink);
+  // Send the CSV data to the server
+  fetch('/submit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ name, email, message })
+  })
+  .then(response => {
+    if (response.ok) {
+      console.log('Form data submitted successfully.');
+    } else {
+      console.error('Failed to submit form data.');
+    }
+  })
+  .catch(error => {
+    console.error('Error submitting form data:', error);
+  });
 });
