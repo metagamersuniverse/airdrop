c8f9c590ed7e5cde2eba8c217a3f5ce5c7e445fd
SSSS <113474082+metagamersuniverse@users.noreply.github.com>
2023-06-17 22:35:45 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,36 @@
 document.getElementById("myForm").addEventListener("submit", submitForm);
 
 function submitForm(event) {
-    event.preventDefault(); // Prevent the form from submitting normally
-    
-    // Get the form values
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var message = document.getElementById("message").value;
-    
-    // Create the Google Form URL
-    var formURL = "https://docs.google.com/forms/d/e/1FAIpQLSdHyXM4DwIHt8wYODQ1ZuefEII_YHkA98JCeZrH8KrvbQUKvA/formResponse";
-    
-    // Construct the form data
-    var formData = new FormData();
-    formData.append("entry.1421637405", name);
-    formData.append("entry.1126696729", email);
-    formData.append("entry.1332288966", message);
-    
-    // Create a new XMLHttpRequest object
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", formURL);
-    
-    // Set the appropriate headers for the form data
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    
-    // Handle the response
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            alert("Form submitted successfully");
-            // You can perform additional actions here after successful submission
-        } else {
-            alert("Form submission failed");
-            // You can handle the failure scenario here
-        }
-    };
-    
-    xhr.onerror = function() {
-        alert("An error occurred");
-    };
-    
-    // Send the form data
-    xhr.send(new URLSearchParams(formData));
+  event.preventDefault(); // Prevent the form from submitting normally
+
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  var formData = {
+    name: name,
+    email: email,
+    message: message
+  };
+
+  // Make a POST request to your backend server
+  fetch('http://203.161.57.175:3000/submit-form', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(function (response) {
+      if (response.ok) {
+        alert("Form submitted successfully");
+        // Additional actions after successful form submission
+      } else {
+        alert("Form submission failed");
+        // Handle failure scenario
+      }
+    })
+    .catch(function (error) {
+      alert("An error occurred: " + error);
+    });
 }
